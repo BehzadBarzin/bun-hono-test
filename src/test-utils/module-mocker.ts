e51a0cd1a483a01668baf8c32bb4431457a181c6
Behzad Barzin <behzad_barzin@live.com>
2024-09-29 14:53:08 +0330

    Adds originalExport to module-mocker factory

--- src/test-utils/module-mocker.ts
+++ src/test-utils/module-mocker.ts
@@ -4,17 +4,20 @@ import { mock } from 'bun:test';
  * A helper function to mock a module and return a function to restore the original module export.
  *
  * @param modulePath module name or path to be mocked (⚠️NOTE: must be relative to this file `src/test-utils/module-mocker.ts`)
- * @param factory the function that returns the mocked export
+ * @param factory the function that returns the mocked export. It will be called with the original export as an argument if we want to access the original implementations.
  * @returns a function to restore the original module export
  */
-export const mockModule = async (modulePath: string, factory: () => Record<string, any>) => {
+export const mockModule = async (
+  modulePath: string,
+  factory: (originalExport: any) => Record<string, any>,
+) => {
   // Get the original export of the module
   const originalExport = {
     ...(await import(modulePath)),
   };
 
   // Get the mocked export
-  const mockExport = factory();
+  const mockExport = factory(originalExport);
 
   // Merge the original and mocked exports
   const result = {
